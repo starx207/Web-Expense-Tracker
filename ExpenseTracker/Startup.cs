37e755b9e7633944dcafe1f06da35b5dcdaddffb
Andrew Starr <starx207@hotmail.com>
2017-10-11 20:08:11 -0400

    Register BudgetContext, Budget, and IBudget for dependency injection

--- ExpenseTracker/Startup.cs
+++ ExpenseTracker/Startup.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using ExpenseTracker.Data;
+using ExpenseTracker.Data.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,6 +24,9 @@ namespace ExpenseTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<BudgetContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IBudget, Budget>();
             services.AddMvc();
         }
 
