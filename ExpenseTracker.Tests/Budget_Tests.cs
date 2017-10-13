476458501a3f12367fc38ac3c540209350d26eb9
Andrew Starr <astarr@appraisalresearch.cc>
2017-10-13 12:07:02 -0400

    Changed name of IBudgetAccess from repo to repository

--- ExpenseTracker.Tests/Budget_Tests.cs
+++ ExpenseTracker.Tests/Budget_Tests.cs
@@ -11,7 +11,7 @@ namespace ExpenseTracker.Tests
     [TestClass]
     public class Budget_Tests
     {
-        private IBudgetAccess repo;
+        private IBudgetAccess repository;
 
         [TestInitialize]
         public void InitializeBudgetAccessObject() {
