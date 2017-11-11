4b6471e6d1bad6c63142005bba66b5c7a514a8fc
Andrew Starr <starx207@hotmail.com>
2017-11-11 10:47:50 -0500

    Moved to ExpenseTracker.Tests.Models namespace

--- ExpenseTracker.Tests/Models/BudgetCategoryValidation_Tests.cs
+++ ExpenseTracker.Tests/Models/BudgetCategoryValidation_Tests.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace ExpenseTracker.Tests
+namespace ExpenseTracker.Tests.Models
 {
     [TestClass]
     public class BudgetCategoryValidation_Tests
