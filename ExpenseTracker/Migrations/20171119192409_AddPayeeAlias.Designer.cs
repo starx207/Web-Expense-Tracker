598e12817d3e8179d79a9a82de24b7188825c994
Andrew Starr <starx207@hotmail.com>
2017-11-19 15:58:15 -0500

    New migration for AddPayeeAlias

--- ExpenseTracker/Migrations/20171119192409_AddPayeeAlias.Designer.cs
+++ ExpenseTracker/Migrations/20171119192409_AddPayeeAlias.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace ExpenseTracker.Migrations
 {
     [DbContext(typeof(BudgetContext))]
-    partial class BudgetContextModelSnapshot : ModelSnapshot
+    [Migration("20171119192409_AddPayeeAlias")]
+    partial class AddPayeeAlias
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ExpenseTracker/Migrations/20171119192409_AddPayeeAlias.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ExpenseTracker.Migrations
+{
+    public partial class AddPayeeAlias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alias",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    PayeeID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alias", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Alias_Payees_PayeeID",
+                        column: x => x.PayeeID,
+                        principalTable: "Payees",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alias_PayeeID",
+                table: "Alias",
+                column: "PayeeID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alias");
+        }
+    }
+}
