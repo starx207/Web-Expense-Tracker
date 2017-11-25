2dbf5f1888478a95782665beba47ebdc01f1aa27
Andrew Starr <starx207@hotmail.com>
2017-11-25 18:06:29 -0500

    New tests for model

---
+++ ExpenseTracker.Tests/Models/AliasValidation_Tests.cs
@@ -0,0 +1,59 @@
+using ExpenseTracker.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Tests.Models
+{
+    [TestClass]
+    public class AliasValidation_Tests
+    {
+        private Alias _model;
+
+        [TestInitialize]
+        public void CreateValidAlias() {
+            _model = new Alias {
+                ID = 1,
+                Name = "Valid alias name",
+                PayeeID = 4
+            };
+        }
+
+        [TestMethod]
+        public void NamePropertyRequired() {
+            _model.Name = null;
+
+            bool isModelStateValid = ValidateModel(_model);
+
+            Assert.IsFalse(isModelStateValid, "Alias Name should not be nullable");
+        }
+
+        [DataTestMethod]
+        [DataRow("qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiop")]
+        [DataRow("")]
+        [DataRow("   ")]
+        public void NamePropertyValidated(string name) {
+            _model.Name = name;
+            string errorMsg = "";
+            bool isModelStateValid = ValidateModel(_model);
+
+            if (name.Length > 100) {
+                errorMsg = "Alias name cannot be greater than 100 characters";
+            } else if (name.Length == 0) {
+                errorMsg = "Alias name cannot be empty";
+            } else {
+                errorMsg = $"Alias name cannot be '{name}'";
+            }
+
+            Assert.IsFalse(isModelStateValid, errorMsg);
+        }
+
+        private bool ValidateModel(Alias model) {
+            var context = new ValidationContext(model, null, null);
+            var results = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(model, context, results, true);
+        }
+    }
+}
---
+++ ExpenseTracker.Tests/Models/PayeeValidation_Tests.cs
@@ -0,0 +1,60 @@
+using ExpenseTracker.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExpenseTracker.Tests.Models
+{
+    [TestClass]
+    public class PayeeValidation_Tests
+    {
+        private Payee _model;
+
+        [TestInitialize]
+        public void CreateValidPayee() {
+            _model = new Payee {
+                ID = 1,
+                Name = "Valid payee name",
+                BeginEffectiveDate = new DateTime(2017, 12, 12),
+                EndEffectiveDate = new DateTime(2017, 12, 30)
+            };
+        }
+
+        [TestMethod]
+        public void NamePropertyRequired() {
+            _model.Name = null;
+
+            bool isModelStateValid = ValidateModel(_model);
+
+            Assert.IsFalse(isModelStateValid, "Payee Name should not be nullable");
+        }
+
+        [DataTestMethod]
+        [DataRow("qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiop")]
+        [DataRow("")]
+        [DataRow("   ")]
+        public void NamePropertyValidated(string name) {
+            _model.Name = name;
+            string errorMsg = "";
+            bool isModelStateValid = ValidateModel(_model);
+
+            if (name.Length > 100) {
+                errorMsg = "Payee name cannot be greater than 100 characters";
+            } else if (name.Length == 0) {
+                errorMsg = "Payee name cannot be empty";
+            } else {
+                errorMsg = $"Payee name cannot be '{name}'";
+            }
+
+            Assert.IsFalse(isModelStateValid, errorMsg);
+        }
+
+        private bool ValidateModel(Payee model) {
+            var context = new ValidationContext(model, null, null);
+            var results = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(model, context, results, true);
+        }
+    }
+}
