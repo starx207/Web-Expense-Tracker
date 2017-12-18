317151b85868a257520ee311e3596dc8fef440dc
Andrew Starr <starx207@hotmail.com>
2017-12-17 21:26:24 -0500

    Initial Commit

---
+++ ExpenseTracker/Repository/ExceptionTypes/RepositoryExceptions.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace ExpenseTracker.Repository
+{
+    [Serializable]
+    public class IdNotFoundException : Exception
+    {
+        public IdNotFoundException() { }
+        public IdNotFoundException(string message) : base(message) { }
+        public IdNotFoundException(string message, Exception inner) : base(message, inner) { }
+        protected IdNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+
+    [Serializable]
+    public class NullIdException : Exception
+    {
+        public NullIdException() { }
+        public NullIdException(string message) : base(message) { }
+        public NullIdException(string message, Exception inner) : base(message, inner) { }
+        protected NullIdException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+
+    [Serializable]
+    public class IdMismatchException : Exception
+    {
+        public IdMismatchException() { }
+        public IdMismatchException(string message) : base(message) { }
+        public IdMismatchException(string message, Exception inner) : base(message, inner) { }
+        protected IdMismatchException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
