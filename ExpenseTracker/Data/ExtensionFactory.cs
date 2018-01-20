2cf71ccfcf004acaeef1f3d7d0cd59652cb71ea2
Andrew Starr <starx207@hotmail.com>
2018-01-19 22:59:13 -0500

    Made Funcs public instead of internal for mocking

--- ExpenseTracker/Data/ExtensionFactory.cs
+++ ExpenseTracker/Data/ExtensionFactory.cs
@@ -10,10 +10,10 @@ namespace ExpenseTracker.Data
 {
     public static class ExtensionFactory
     {
-        internal static Func<IQueryable<BudgetCategory>, ICategoryExtMask> CategoryExtFactory { get; set; }
-        internal static Func<IQueryable<Payee>, IPayeeExtMask> PayeeExtFactory { get; set; }
-        internal static Func<IQueryable<Alias>, IAliasExtMask> AliasExtFactory { get; set; }
-        internal static Func<IQueryable<Transaction>, ITransactionExtMask> TransactionExtFactory { get; set; }
+        public static Func<IQueryable<BudgetCategory>, ICategoryExtMask> CategoryExtFactory { get; set; }
+        public static Func<IQueryable<Payee>, IPayeeExtMask> PayeeExtFactory { get; set; }
+        public static Func<IQueryable<Alias>, IAliasExtMask> AliasExtFactory { get; set; }
+        public static Func<IQueryable<Transaction>, ITransactionExtMask> TransactionExtFactory { get; set; }
 
         static ExtensionFactory() {
             CategoryExtFactory = col => new CategoryExt(col);
