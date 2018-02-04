43f222f06e832dcfb72a9b646e2611f640dcc089
Andrew Starr <starx207@hotmail.com>
2018-02-04 15:22:40 -0500

    Added tests for Edit POST

--- ExpenseTracker.Tests/Controllers/AliasController_Tests.cs
+++ ExpenseTracker.Tests/Controllers/AliasController_Tests.cs
@@ -3,7 +3,6 @@ using ExpenseTracker.Models;
 using ExpenseTracker.Services;
 using ExpenseTracker.TestResources;
 using Microsoft.AspNetCore.Mvc;
-// using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
@@ -16,33 +15,10 @@ namespace ExpenseTracker.Controllers.Tests
     [TestClass]
     public class AliasController_Tests : TestCommon
     {
-//         private IBudgetService budget;
-//         private Dictionary<int, string> aliasReference;
         private AliasController controller;
         private Mock<IAliasManagerService> mockService;
         private readonly string payeeListKey = "PayeeList";
-//         private readonly string payeeSelectListKey = "PayeeList";
 
-//         private Mock<IBudgetService> mockBudget;
-
-//         [TestInitialize]
-//         public void InitializeTestData() {
-//             List<BudgetCategory> categories = TestInitializer.CreateTestCategories();
-//             List<Payee> payees = TestInitializer.CreateTestPayees(categories.AsQueryable());
-//             List<Alias> aliases = TestInitializer.CreateTestAliases(payees.AsQueryable());
-//             mockBudget = new Mock<IBudgetService>();
-//             mockBudget.Setup(m => m.AddAlias(It.IsAny<Alias>()));
-//             mockBudget.Setup(m => m.GetAliases()).Returns(new TestAsyncEnumerable<Alias>(aliases));
-//             mockBudget.Setup(m => m.GetPayees()).Returns(payees.AsQueryable());
-//             budget = mockBudget.Object;
-
-//             aliasReference = new Dictionary<int, string>();
-//             foreach (var alias in budget.GetAliases()) {
-//                 aliasReference.Add(alias.ID, alias.Name);
-//             }
-
-//             controller = new AliasController(budget);
-//         }
         [TestInitialize]
         public void Initialize_test_objects() {
             mockService = new Mock<IAliasManagerService>();
@@ -266,70 +242,107 @@ namespace ExpenseTracker.Controllers.Tests
                     controller.Edit(1));
             }
 
-//             [TestMethod]
-//             public async Task EditPOSTWithValidModelState() {
-//                 Alias aliasToEdit = budget.GetAliases().First();
-//                 int testID = aliasToEdit.ID;
-//                 string newName = aliasToEdit.Name + "_modified";
-//                 aliasToEdit.Name = newName;
-//                 IActionResult actionResult = await controller.Edit(aliasToEdit.ID, aliasToEdit);
-//                 var result = actionResult as RedirectToActionResult;
+            [TestMethod]
+            public async Task Edit_POST_calls_UpdateAliasAsync_and_redirects_to_payee_index() {
+                // Arrange
+                var alias = new Alias();
 
-//                 Assert.IsNotNull(result, "Edit POST did not return a RedirectToActionResult");
-//                 Assert.AreEqual("Index", result.ActionName, $"Edit POST should redirect to 'Index', not {result.ActionName}");
-//                 Assert.AreEqual(nameof(Payee), result.ControllerName, "Edit POST shoud redirect to Payee Index");
+                // Act
+                var result = await controller.Edit(1, alias);
+                var redirectResult = result as RedirectToActionResult;
 
-//                 Alias editedAlias = budget.GetAliases().Where(p => p.ID == testID).First();
+                // Assert
+                mockService.Verify(m => m.UpdateAliasAsync(1, alias), Times.Once());
+                Assert.IsNotNull(redirectResult);
+                Assert.AreEqual("Payee", redirectResult.ControllerName);
+                Assert.AreEqual("Index", redirectResult.ActionName);
+            }
 
-//                 Assert.AreEqual(newName, editedAlias.Name, "The changes for the alias were not saved");
-//             }
+            [TestMethod]
+            public async Task Edit_POST_returns_NotFound_when_IdMismatchException_thrown() {
+                // Arrange
+                var alias = new Alias();
+                mockService.Setup(m => m.UpdateAliasAsync(It.IsAny<int>(), It.IsAny<Alias>())).ThrowsAsync(new IdMismatchException());
 
-//             [TestMethod]
-//             public async Task EditPOSTWithMismatchedIDReturnsNotFound() {
-//                 Alias editedAlias = budget.GetAliases().First();
-//                 editedAlias.Name += "_modified";
-//                 IActionResult actionResult = await controller.Edit(editedAlias.ID + 1, editedAlias);
+                // Act
+                var result = await controller.Edit(1, alias);
 
-//                 Assert.IsInstanceOfType(actionResult, typeof(NotFoundResult), "If the ID doesn't match the ID of the edited Alias, Not Found should be called");
-//             }
+                // Assert
+                Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+            }
 
-//             [TestMethod]
-//             public async Task EditPOSTWithInvalidModelStateReturnsToEditView() {
-//                 Alias editedAlias = budget.GetAliases().First();
-//                 string newName = editedAlias.Name + "_modified";
-//                 editedAlias.Name = newName;
+            [TestMethod]
+            public async Task Edit_POST_returns_NotFound_when_ConcurrencyException_thrown_and_alias_does_not_exist() {
+                // Arrange
+                var alias = new Alias();
+                mockService.Setup(m => m.UpdateAliasAsync(It.IsAny<int>(), It.IsAny<Alias>())).ThrowsAsync(new ConcurrencyException());
+                mockService.Setup(m => m.AliasExists(It.IsAny<int>())).Returns(false);
 
-//                 controller.ModelState.AddModelError("test", "test");
-//                 IActionResult actionResult = await controller.Edit(editedAlias.ID, editedAlias);
+                // Act
+                var result = await controller.Edit(1, alias);
 
-//                 var result = actionResult as ViewResult;
-//                 Assert.IsNotNull(result, "Edit POST with invalid model state should return a ViewResult");
-//                 Assert.AreEqual("Edit", result.ViewName, $"Edit POST with invalid model state should return to 'Edit' view, not '{result.ViewName}'");
+                // Assert
+                Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+            }
 
-//                 Alias model = (Alias)result.Model;
+            [TestMethod]
+            public async Task Edit_POST_rethrows_ConcurrencyException_when_alias_exists() {
+                // Arrange
+                var alias = new Alias();
+                mockService.Setup(m => m.UpdateAliasAsync(It.IsAny<int>(), It.IsAny<Alias>())).ThrowsAsync(new ConcurrencyException());
+                mockService.Setup(m => m.AliasExists(It.IsAny<int>())).Returns(true);
 
-//                 Assert.AreEqual(editedAlias.ID, model.ID, "The wrong alias was passed back to the View");
+                // Act & Assert
+                await Assert.ThrowsExceptionAsync<ConcurrencyException>(() =>
+                    controller.Edit(1, alias));
+            }
 
-//                 Assert.AreEqual(newName, model.Name, "The updated values were not preserved when returning to the View");
-//             }
+            [TestMethod]
+            public async Task Edit_POST_throws_exceptions_not_of_type_IdMismatch() {
+                // Arrange
+                var alias = new Alias();
+                mockService.Setup(m => m.UpdateAliasAsync(It.IsAny<int>(), It.IsAny<Alias>())).ThrowsAsync(new Exception());
 
-//             [TestMethod]
-//             public async Task EditPOSTWithInvalidModelStatePopulatesPayeeSelect() {
-//                 Alias editedAlias = budget.GetAliases().First();
-//                 Payee newPayee = budget.GetPayees().Where(p => p.ID != editedAlias.PayeeID).First();
-//                 editedAlias.Name += "_modified";
-//                 editedAlias.AliasForPayee = newPayee;
-//                 editedAlias.PayeeID = newPayee.ID;
+                // Act & Assert
+                await Assert.ThrowsExceptionAsync<Exception>(() =>
+                    controller.Edit(1, alias));
+            }
 
-//                 controller.ModelState.AddModelError("test", "test");
-//                 IActionResult actionResult = await controller.Edit(editedAlias.ID, editedAlias);
-//                 var result = actionResult as ViewResult;
+            [TestMethod]
+            public async Task Edit_POST_returns_to_view_with_correct_model_when_ModelState_is_invalid() {
+                // Arrange
+                var alias = new Alias();
+                controller.ModelState.AddModelError("test", "test");
 
-//                 // Check that ViewData is not null
-//                 AssertThatViewDataIsSelectList(result.ViewData, payeeSelectListKey, budget.GetPayees().Select(p => p.ID.ToString()), newPayee.ID.ToString());
-//             }
+                // Act
+                var result = await controller.Edit(1, alias);
+                var viewResult = result as ViewResult;
+                var model = viewResult.Model as Alias;
 
-//             // TODO: Figure out how to test the DbUpdateConcurrencyException portion of Edit POST
+                // Assert
+                Assert.IsNotNull(viewResult);
+                Assert.AreEqual("Edit", viewResult.ViewName);
+                Assert.AreSame(alias, model);
+            }
+
+            [TestMethod]
+            public async Task Edit_POST_correctly_populates_select_list_when_ModelState_invalid() {
+                // Arrange
+                var payees = new List<Payee> {
+                    new Payee { ID = 1 },
+                    new Payee { ID = 2 },
+                    new Payee { ID = 3 }
+                }.AsQueryable();
+                var alias = new Alias { PayeeID = 3 };
+                controller.ModelState.AddModelError("test", "test");
+                mockService.Setup(m => m.GetOrderedPayees(It.IsAny<string>(), It.IsAny<bool>(), It.IsAny<bool>())).Returns(payees);
+
+                // Act
+                var result = (ViewResult)(await controller.Edit(1, alias));
+
+                // Assert
+                AssertThatViewDataIsSelectList(result.ViewData, payeeListKey, payees.Select(p => p.ID.ToString()), alias.PayeeID.ToString());
+            }
         #endregion
     }
 }
